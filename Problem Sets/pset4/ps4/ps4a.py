1bc3ebd2cf23c56d99b8627d11490ca676b40861
vercetti322 <vercettitommy322@gmail.com>
2023-08-15 11:24:46 +0530

    pset 4a is done!!

--- Problem Sets/pset4/ps4/ps4a.py
+++ Problem Sets/pset4/ps4/ps4a.py
@@ -1,6 +1,6 @@
 # Problem Set 4A
-# Name: <your name here>
-# Collaborators:
+# Name: Jatin
+# Collaborators: None
 # Time Spent: x:xx
 
 def get_permutations(sequence):
@@ -22,19 +22,26 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
+    list = []
+    # Base case
+    if len(sequence) == 1:
+        return [sequence]
+    
+    # Recursive case
+    else:
+        for permutation in get_permutations(sequence[1 : ]):
+            for i in range(len(permutation) + 1):
+                new_permutation = permutation[ : i] + sequence[0] + permutation[i : ]
+                list.append(new_permutation)
+    return list
+            
 
 if __name__ == '__main__':
 #    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
+   example_input = 'abcd'
+   print('Input:', example_input)
+   print('Actual Output:', get_permutations(example_input))
     
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
-
-    pass #delete this line and replace with your code here
-
